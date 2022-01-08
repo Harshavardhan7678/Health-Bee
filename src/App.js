c39c23d71258d19b525349aaee4a36e3a77e9755
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-08 18:31:13 +0530

    feat: landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import Footer from "./Components/footer/Footer.jsx";
-// import Navbar from "./Components/Navbar.jsx";
+import Navbar from "./Components/Navbar.jsx";
 import Landing from "./Pages/Landing/Landing.jsx";
-// import About from "./Pages/About/About.jsx"
-// import Calculator from "./Pages/Calculator/Calculator.jsx";
-// import Blog from "./Pages/Blog/Blog.jsx";
-// import Yoga from "./Pages/Yoga/Yoga.jsx";
-// import Challenge from "./Pages/Challenge/Challenge.jsx";
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from "./Pages/About/About.jsx"
+import Calculator from "./Pages/Calculator/Calculator.jsx";
+import Blog from "./Pages/Blog/Blog.jsx";
+import Yoga from "./Pages/Yoga/Yoga.jsx";
+import Challenge from "./Pages/Challenge/Challenge.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-    <Landing/>
-<Footer/>
 
-      {/* <Router>
+      <Router>
         <Navbar />
         <Routes />
         <Route exact path="/">
@@ -38,7 +36,7 @@ function App() {
         </Route>
         <Routes />
         <Footer />
-      </Router> */}
+      </Router>
 
     </>
   );
