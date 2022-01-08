edde5b62426755d78f42257b53dd1a8367fd5d8a
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-09 00:07:03 +0530

    feat: landing page enhanced

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Features from "./features/features";
+import Hero from "./hero/hero";
 import Service from "./services/services";
 import Stats from "./stats/stats";
 
 export default function Landing() {
   return (
     <div>
+      <Hero/>
       <Stats />
       <Features />
       <Service/>
--- src/Pages/Landing/services/servicedata.js
+++ src/Pages/Landing/services/servicedata.js
@@ -4,24 +4,28 @@ const serviceData = [
     image: "https://res.cloudinary.com/dzbdnlr0f/image/upload/v1641645167/hibernation-hack/13082_w3n709.jpg",
     name: "Yoga",
     location: "",
+    link: '/yoga',
   },
   {
     id: 2,
     image: "https://res.cloudinary.com/dzbdnlr0f/image/upload/v1641645160/hibernation-hack/3125988_imnmqc.jpg",
     name: "Blog",
     location: "",
+    link: '/blog',
   },
   {
     id: 3,
     image: "https://res.cloudinary.com/dzbdnlr0f/image/upload/v1641645159/hibernation-hack/5473851_xjeppm.jpg",
     name: "21 Days challenge",
     location: "",
+    link: '/challenge',
   },
   {
     id: 4,
     image: "https://res.cloudinary.com/dzbdnlr0f/image/upload/v1641645293/hibernation-hack/145411-OTQZ7A-227_iv14ki.jpg",
     name: "Health calculator",
     location: "",
+    link: '/calculator',
   },
   
  
