d669574399d69cd39179719b03b6708e73a84ceb
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-01-09 13:21:08 +0300

    Changes in Yoga

--- src/Pages/Yoga/Yoga.jsx
+++ src/Pages/Yoga/Yoga.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Countdown from './Countdown';
 import './yoga.css'
@@ -6,18 +5,19 @@ import './yoga.css'
 export default function Yoga() {
 
     return (
+
         <div className="mainMeditationBox">
-        <div className='timerBack'>
-          <Countdown/>
-        {/* <Lottie animationData={timerback} 
+            <div className='timerBack'>
+                <Countdown />
+                {/* <Lottie animationData={timerback} 
                 style={{
                     height:700
                 }}
             /> */}
-        </div>
-        {/* {show && <OpeningMessage/>} */}
-        <div className="spotifyPlayer">
-            <iframe title="yoga" src="https://open.spotify.com/embed/playlist/4mfobAcyw052F12K4vwtoW?utm_source=generator" width="50%" height="500" frameBorder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+            {/* {show && <OpeningMessage/>} */}
+            <div className="spotifyPlayer">
+                <iframe title="yoga" src="https://open.spotify.com/embed/playlist/4mfobAcyw052F12K4vwtoW?utm_source=generator" width="50%" height="500" frameBorder="0" allowTransparency="true" allow="encrypted-media"></iframe>
             </div>
         </div>
         // {/* <div className='yoga'>
