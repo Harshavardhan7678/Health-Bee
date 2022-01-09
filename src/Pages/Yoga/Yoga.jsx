c489a0af7d471a6d304e84640fc7c25c99739712
a-sambhab <saisambhav21chaini@gmail.com>
2022-01-09 14:22:34 +0530

    Yoga page

--- src/Pages/Yoga/Yoga.jsx
+++ src/Pages/Yoga/Yoga.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Countdown from './Countdown';
+import './yoga.css'
 
 export default function Yoga() {
+
     return (
-        <div>
-            <h1>Yoga Page</h1>
+        <div className="mainMeditationBox">
+        <div className='timerBack'>
+          <Countdown/>
+        {/* <Lottie animationData={timerback} 
+                style={{
+                    height:700
+                }}
+            /> */}
         </div>
-    )
+        {/* {show && <OpeningMessage/>} */}
+        <div className="spotifyPlayer">
+            <iframe src="https://open.spotify.com/embed/playlist/4mfobAcyw052F12K4vwtoW?utm_source=generator" width="50%" height="500" frameBorder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+        </div>
+        // {/* <div className='yoga'>
+        //     <Countdown/>
+        //     <div className="spotifyPlayer">
+        //         <iframe src="https://open.spotify.com/embed/playlist/4mfobAcyw052F12K4vwtoW?utm_source=generator" width="50%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        //     </div>
+        // </div> */}
+    );
 }
